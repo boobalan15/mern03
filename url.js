6b93edae26489d4f63453932ef59852963da6496
boobalan15 <119777416+boobalan15@users.noreply.github.com>
2023-08-25 23:35:08 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,2 +1,3 @@
-module.exports = `mongodb://127.0.0.1:27017`
+module.exports = `mongodb+srv://admin:admin@cluster0.cfdib2w.mongodb.net/?retryWrites=true&w=majority`
+// module.exports = `mongodb://127.0.0.1:27017`
 //module.exports = `mongodb+srv://admin:admin@mdb.vtkja.mongodb.net`
